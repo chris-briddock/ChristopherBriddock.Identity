6bdfbf321ef8344dcdbb4a5ec96c97337d8ce9c8
Christopher Briddock <christopherbriddock@outlook.com>
2024-01-17 01:38:20 +0000

    Prepare to add tests. Move LICENSE, README and .gitignore

---
+++ src/ChristopherBriddock.Service.Identity.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace ChristopherBriddock.Service.Identity.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+    }
+}
