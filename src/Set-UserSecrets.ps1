aa129597e256f53b22336eb8cfc0505b1f3e2ffe
Christopher Briddock <christopherbriddock@outlook.com>
2024-01-23 00:57:53 +0000

    Add test runner script, which can generate a code coverage report, or just run the tests.

--- src/Set-UserSecrets.ps1
+++ src/Set-UserSecrets.ps1
@@ -1,8 +1,8 @@
 $currentPath = (Get-Location).Path
 
-if ($currentPath -notcontains "src/ChristopherBriddock.Service.IdentityServer")
+if ($currentPath -notcontains "src/ChristopherBriddock.Service.Identity")
 {
-    Set-Location "src/ChristopherBriddock.Service.IdentityServer"
+    Set-Location "src/ChristopherBriddock.Service.Identity"
 }
 
 dotnet user-secrets set "ConnectionStrings:Default" "Host=172.16.1.8;Port=5432;Username=pguser;Password=drRhxp397NP4__;Database=ChristopherBriddock.Service.Identity;"
